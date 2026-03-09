5997bf8c21d3e5fc8ad6a0eb5b01f38763484ba2
itzjunayed <junayed12sents@gmail.com>
2026-03-09 19:26:07 +0600

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Sketch Frenzy Server Running");
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
